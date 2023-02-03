12a65d1472b671b58ea04905dcc1a1396c4dabe8
Yana <goyana72@gmail.com>
2023-02-03 21:35:32 +0300

    проект туду без граф дизайна

--- todo.js
+++ todo.js
@@ -1,33 +1,66 @@
+//todo list
 "use strict"
-    //объекты 
-    
-    
-    const phoneBook = {
-        contacks: {
-            "Vanya": 89183838383,
-            "Sasha": 89187374748,
-            "Rita": 74383773384
-    
-        },
-        //метод удаления 
-        del(name, number) {
-            // this.contacks[name] = number;
-            delete phoneBook.contacks[name];
-    
-        },//метод добавления    
-        add(name, number) {
-            this.contacks[name] = number;
-        }
-    };
-    //вызовы методов
-    phoneBook.del('Rita', 74383773384);
-    console.log(phoneBook);
-    phoneBook.add('Ilya', 37429874373);
-    console.log(phoneBook);
-    
-    // //вывод имен и номеров в формате "имя - номер"
-    
-    for (const name in phoneBook.contacks) {
-        console.log(`${name} - ${phoneBook.contacks[name]}`)
-    };
+const toDo = "to do";
+const inProgress = 'in progress';
+const done = 'done'
 
+const todo = {
+    list: {
+        'brush your teeth': done,
+        "create a new practice task": inProgress,
+        "make a bed": done,
+        "write a post": toDo,
+    },
+
+    changeStatus(task, status) {
+        if (task in todo.list) {
+            todo.list[task] = status;
+            console.log(`you add a status '${status}' in the task '${task}'`);
+        } else if (!(task in todo.list)) {
+            console.log('a task is not defined');
+        }
+    }, addTask(task, status) {
+        if (task in todo.list) {
+            console.log(`task '${task}' is already use`);
+        } else if (!(task in todo.list)) {
+            todo.list[task] = status;
+            console.log(`task '${task}' has been created`);
+        }
+    }, deleteTask(task) {
+        delete todo.list[task];
+        console.log(`task '${task}' has been deleted`);
+    }, showList() {
+        console.log('To Do:');
+        for (const show in todo.list) {
+
+            if (todo.list[show] === toDo) {
+                console.log(`  ${show};`);
+            };
+        };
+        console.log('In Progress:');
+        for (const show in todo.list) {
+
+            if (todo.list[show] === inProgress) {
+                console.log(`  ${show};`);
+            };
+        };
+        console.log('Done:');
+        for (const show in todo.list) {
+
+            if (todo.list[show] === done) {
+                console.log(`  ${show};`);
+            };
+        };
+    }
+};
+
+
+
+todo.changeStatus("write a post", done);
+console.log(todo.list);
+todo.addTask('have a walk');
+console.log(todo.list);
+todo.deleteTask('have a walk');
+console.log(todo.list)
+
+todo.showList();
